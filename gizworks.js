1ae0a69b44c3b6325f8c3d840dbc55bc6147458a
Taylor <tvance989@gmail.com>
2014-08-08 12:00:57 -0500

    More views and routes. Also, you may test the autoscroll feature by clicking About in the unordered list.

--- gizworks.js
+++ gizworks.js
@@ -14,6 +14,30 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
     res.render('home');
 });
+app.get('/about', function(req, res){
+    res.render('about');
+});
+app.get('/gallery', function(req, res){
+    res.render('gallery');
+});
+app.get('/order', function(req, res){
+    res.render('order');
+});
+app.get('/contact', function(req, res){
+    res.render('contact');
+});
+
+// 404 catch-all handler
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+// 500 error handler
+app.use(function(req, res, next){
+    console.error(err.stack)
+    res.status(500);
+    res.render('500');
+});
 
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:'+app.get('port')+'; press Ctrl-C to terminate.');
